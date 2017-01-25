26b97f3f900a66f01e5fc43213195a0ba28780d0
henrique_iv <henriqueindalencio@gmail.com>
2017-01-25 02:57:06 -0200

    Image to indicate the status

--- Wifi/ViewController.swift
+++ Wifi/ViewController.swift
@@ -6,12 +6,28 @@
 //  Copyright © 2017 Henrique Valcanaia. All rights reserved.
 //
 
+import ReachabilitySwift
 import UIKit
 
 class ViewController: WifiViewController {
     
+    override var reachabilityStatus: Reachability.NetworkStatus {
+        didSet {
+            switch self.reachabilityStatus {
+            case .notReachable:
+                image.image = UIImage(named: "noconnection")
+            case .reachableViaWiFi:
+                image.image = UIImage(named: "wifi")
+            case .reachableViaWWAN:
+                image.image = UIImage(named: "cellular")
+            }
+        }
+    }
+    
+    @IBOutlet weak var image: UIImageView!
+    
     @IBAction func didTapCheckButton(_ sender: Any) {
-        print("Is on wifi? \(self.wifi)")
+        print("Is on wifi? \(wifi ?? false)")
     }
     
     
